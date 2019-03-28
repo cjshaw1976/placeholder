f965dab221f225fff00ba267576869c5606d9016
Christopher J Shaw <chris@shawcando.com>
2019-03-28 10:29:32 +0200

    Collecting Stats On Live
    Implemented Collecting stats on live site

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'e4v0)wjvrscqjc-_!mi2zngc_onvz*(b^i*s&%2t3-&k!ac3x('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'stats',
 ]
 
 MIDDLEWARE = [
@@ -47,6 +48,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'stats.middleware.LogRequestMiddleware',
 ]
 
 ROOT_URLCONF = 'mysite.urls'
