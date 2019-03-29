c40da40e89a8d250c12b8a20ee3e192e940dcdb0
Christopher J Shaw <chris@shawcando.com>
2019-03-29 10:08:30 +0200

    Added stats to admin

--- stats/admin.py
+++ stats/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import DataLog, LocationLog, VisitorLog
+
+admin.site.register(DataLog)
+admin.site.register(LocationLog)
+admin.site.register(VisitorLog)
